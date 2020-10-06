777bb83ac22c9417a88c355ce6c4b3951820abf1
kealanheena <kealanheena@gmail.com>
2020-10-06 04:25:03 +0100

    test: add addiotional test to prevent errors later

--- tests/getReportData.test.js
+++ tests/getReportData.test.js
@@ -12,27 +12,27 @@ describe('#getReportData', () => {
     startDate = new Date('August 1, 2020 12:15:30');
   });
 
-  test('getReportData should be a function', () => {
+  it('should be a function', () => {
     expect(getReportData).toBeInstanceOf(Function);
   });
 
-  describe('When the amount of day is 1', () => {
+  describe('When the amount of days is 1', () => {
     beforeEach(() => {
       amountOfDays = 1;
     });
 
-    test('getReportData should return an array of dates', () => {
+    it(`should return an array with \'${firstDate}\'`, () => {
       expect(getReportData([], amountOfDays, startDate)).toEqual([`${firstDate}`]);
     });
     
-    test('getReportData should take an array of objects and return an array of dates', () => {
+    it('should take an array of customers and return an array of dates with the names of people getting the marketing info', () => {
       expect(getReportData([{
         name: 'jess', 
         preference: { type: 'everyday' }
       }], amountOfDays, startDate)).toEqual([`${firstDate} jess`]);
     });
 
-    test('getReportData should return dates with multiple names if there is multiple customer inputs', () => {
+    it('should return dates with multiple names if there is multiple customer inputs', () => {
       expect(getReportData([{
         name: 'jess', 
         preference: { type: 'everyday' }
@@ -49,19 +49,19 @@ describe('#getReportData', () => {
       amountOfDays = 2;
     });
 
-    test('getReportData should return an array of subsequential dates', () => {
+    it('should return an array of subsequential dates', () => {
       expect(getReportData([], amountOfDays, startDate)).toEqual([`${firstDate}`, `${secondDate}`]);
     });
 
     describe('when the customers Preference is Everyday', () => {
-      test('getReportData should take an array of objects and return an array of dates', () => {
+      it('should take an array of customers and return an array of subsequential dates with the names of people getting the marketing info', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { type: 'everyday' }
         }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate} jess`]);
       });
   
-      test('getReportData should return dates with multiple names if there is multiple customer inputs', () => {
+      it('should return dates with multiple names if there is multiple customer inputs', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { type: 'everyday' }
@@ -73,7 +73,7 @@ describe('#getReportData', () => {
     });
 
     describe('when the customers Preference is Specific days of the week', () => {
-      test('getReportData should only add the customers name to dates on that day', () => {
+      it('should only add the customers name to dates on that day they in their preferences', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { 
@@ -83,7 +83,7 @@ describe('#getReportData', () => {
         }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate}`]);
       });
 
-      test('getReportData should also work if there are Multiple dates in the array', () => {
+      it('should also work if there are multiple dates in the array', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { 
@@ -93,7 +93,7 @@ describe('#getReportData', () => {
         }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate} jess`]);
       });
 
-      test('getReportData should also work if one of the days don\'t appear in the data', () => {
+      it('should also work if one of the days don\'t appear in the data', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { 
@@ -103,10 +103,26 @@ describe('#getReportData', () => {
         }], amountOfDays, startDate)).toEqual([`${firstDate}`, `${secondDate} jess`]);
       });
 
+      it('should return dates with multiple names if there is multiple customer inputs', () => {
+        expect(getReportData([{
+          name: 'jess', 
+          preference: { 
+            type: 'day', 
+            days: ['Saturday']
+          }
+        }, {
+          name: 'ted', 
+          preference: { 
+            type: 'day', 
+            days: ['Monday', 'Sunday']
+          }
+        }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate} ted`]);
+      });
+
     });
 
     describe('when the customers Preference is Specific days of the week', () => {
-      test('getReportData should only add the customers name to dates in prefernces dates', () => {
+      it('should only add the customers name to dates in prefernces dates', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { 
@@ -116,7 +132,7 @@ describe('#getReportData', () => {
         }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate}`]);
       });
 
-      test('getReportData should only add the customers name to dates in prefernces dates', () => {
+      it('should only add the customers name to dates in prefernces dates', () => {
         expect(getReportData([{
           name: 'jess', 
           preference: { 
@@ -124,6 +140,41 @@ describe('#getReportData', () => {
             dates: [1, 2]
           }
         }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate} jess`]);
+      });
+
+      it('should also work if one of the days don\'t appear in the data', () => {
+        expect(getReportData([{
+          name: 'jess', 
+          preference: { 
+            type: 'date', 
+            dates: [1, 20, 28]
+          }
+        }], amountOfDays, startDate)).toEqual([`${firstDate} jess`, `${secondDate}`]);
+      });
+
+    });
+
+    describe('when each of the customers preference are different', () => {
+      it('should only add the customers name to dates in prefernces dates', () => {
+        expect(getReportData([{
+          name: 'jess', 
+          preference: { 
+            type: 'date', 
+            dates: [1]
+          }
+        }, {
+          name: 'kerry', 
+          preference: { 
+            type: 'everyday', 
+          },
+        },
+        {
+          name: 'ted',
+          preference: { 
+            type: 'day', 
+            days: ['Sunday']
+          }
+        }], amountOfDays, startDate)).toEqual([`${firstDate} jess, kerry`, `${secondDate} kerry, ted`]);
       });
 
     });
